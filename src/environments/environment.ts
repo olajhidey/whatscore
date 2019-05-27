808f155206cecec6447478da5e5825dcb36f142b
JIDE <olajideawoyinka@gmail.com>
2019-05-27 11:32:10 +0100

    Got to hide some!

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAqQGvcTIUTQzrd2esuTswwNT3JYcT0lcA",
-    authDomain: "whatscore-d96e3.firebaseapp.com",
-    databaseURL: "https://whatscore-d96e3.firebaseio.com",
-    projectId: "whatscore-d96e3",
-    storageBucket: "whatscore-d96e3.appspot.com",
-    messagingSenderId: "514458289149",
-    appId: "1:514458289149:web:121571852990e126"
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXX",
+    databaseURL: "XXXXXXXXXXXXXXXXXXXXXX",
+    projectId: "XXXXXXXXXXXXXXXX",
+    storageBucket: "XXXXXXXXXXXXXXXX",
+    messagingSenderId: "XXXXXXXXXXXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXXXXXXXXXX"
   }
 };
 
